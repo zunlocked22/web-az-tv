98e4def44f34f9819264248007e864f5952c20af
zunlocked22 <a2zunlockedver2@gmail.com>
2025-07-08 15:47:09 +0800

    Update token.js

--- api/token.js
+++ api/token.js
@@ -1,7 +1,7 @@
 // api/token.js
 export default async function handler(req, res) {
   try {
-    const response = await fetch('https://iptv-php-7pns.onrender.com/get-token');
+    const response = await fetch('https://web-aztv.onrender.com/get-token');
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
